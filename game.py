2b0d4f616e2560bec46a97a8da7e652069e161bf
Ash <ashikakambang@gmail.com>
2024-08-27 23:22:42 +0545

    Update game.py

--- game.py
+++ game.py
@@ -32,18 +32,18 @@ print('''
 print("Welcome to Treasure Island.\nYour mission is to find the treasure ")
 
 direction=input("You have come to the centre of the two road!!!\n"
-                "Which side would you like to go?\n 'Left' or 'Right'\n")
+                "Which side would you like to go?\n 'Left' or 'Right'\n").lower()
 
 if direction=="Right":
     print("Game Over\nYou fell into the hole")
 else:
-    decision=input("Do you want to 'swim' or 'wait'?")
+    decision=input("Do you want to 'swim' or 'wait'?").lower()
     if decision=="swim":
         print("Game Over\nYou were killed by the bunch of mouse Trop")
     else:
         door=input("There are three doors which are of \n"
                    "'Red' ,'Blue' and 'Yellow'!\n"
-                   "Choose between these three!!!" )
+                   "Choose between these three!!!" ).lower()
         if door=="Blue":
             print("Game Over\nYou are eaten by the crocodile")
         elif door=="Red":
